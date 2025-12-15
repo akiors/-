992b6bb59d994f06b80c093eecf04831ae57c8e3
akiors <1920763622@qq.com>
2025-12-15 22:57:42 +0800

    Update data.gd

--- code/data.gd
+++ code/data.gd
@@ -1,10 +1,10 @@
-extends Resource
-class_name user_data
-@export var userdata:Dictionary
-@export var dict:Dictionary
-@export var newmoney:Dictionary
-@export var inway:Dictionary
-@export var outway:Dictionary
-@export var budget:Dictionary
-@export var current_budget:Dictionary
-@export var isadmin:Dictionary
+extends Resource                   #继承resource
+class_name user_data               #设置类名为user_data
+@export var userdata:Dictionary    #用户账号密码
+@export var dict:Dictionary        #收入支出记录
+@export var newmoney:Dictionary    #总金额
+@export var inway:Dictionary       #收入类型数据
+@export var outway:Dictionary      #支出类型数据
+@export var budget:Dictionary      #预算
+@export var current_budget:Dictionary    #当前预算
+@export var isadmin:Dictionary     #是否为管理员
