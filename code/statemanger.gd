ee4c8c67c1248eb1c2b23b1c007f77ba11d2fe04
akiors <1920763622@qq.com>
2025-12-15 23:10:23 +0800

    Update statemanger.gd

--- code/statemanger.gd
+++ code/statemanger.gd
@@ -1,10 +1,10 @@
-extends Node
-@export var isload:bool
-@export var user_name:String
-@export var bud:int
-@export var current_bud:int
-@export var isadmin:bool
-var sprite=preload("res://image/Almanac_IndexButton.png")
+extends Node               #全局脚本,在不同场景可以通过远程链接到此节点的相关属性
+@export var isload:bool    #是否登录 
+@export var user_name:String       #当前用户名
+@export var bud:int                #预算
+@export var current_bud:int        #当前预算
+@export var isadmin:bool           #是否为管理员
+var sprite=preload("res://image/Almanac_IndexButton.png")      #预加载图片
 var sprite1=preload("res://image/Almanac_IndexButtonHighlight.png")
 
 func _ready() -> void:
